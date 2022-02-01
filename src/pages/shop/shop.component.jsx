2010fd7af0d08455e9f13078d2751dc48da06767
Mikita Kavaleuski <nikcrafter1999@gmail.com>
2022-02-01 19:08:34 +0300

    Updated shop data with new data (id), Updated cart utils to group items in cart reducer

--- src/pages/shop/shop.component.jsx
+++ src/pages/shop/shop.component.jsx
@@ -1,22 +1,22 @@
-import React from 'react';
+import React from "react";
 
-import SHOP_DATA from './shop.data.js';
+import SHOP_DATA from "./shop.data.js";
 
-import CollectionPreview from '../../components/collection-preview/collection-preview';
+import CollectionPreview from "../../components/collection-preview/collection-preview.component";
 
 class ShopPage extends React.Component {
   constructor(props) {
     super(props);
 
     this.state = {
-      collections: SHOP_DATA
+      collections: SHOP_DATA,
     };
   }
 
   render() {
     const { collections } = this.state;
     return (
-      <div className='shop-page'>
+      <div className="shop-page">
         {collections.map(({ id, ...otherCollectionProps }) => (
           <CollectionPreview key={id} {...otherCollectionProps} />
         ))}
