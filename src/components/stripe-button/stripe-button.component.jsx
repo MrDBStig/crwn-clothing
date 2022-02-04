78b20df72ade1838bb231c5b7c76207f8c1739d4
Mikita Kavaleuski <nikcrafter1999@gmail.com>
2022-02-04 19:48:50 +0300

    Updating Stripe integration, our favicon and title

---
+++ src/components/stripe-button/stripe-button.component.jsx
@@ -0,0 +1,30 @@
+import React from "react";
+import StripeCheckout from "react-stripe-checkout";
+
+const StripeCheckoutButton = ({ price }) => {
+  const priceForStripe = price * 100;
+  const publishableKey =
+    "pk_test_51KPUFpCbjM7ZQ6svWPgiize5ezU3rUOzLxC0GYBaWAutQ6rLeczmiFwIMnJRXyGdYWKWS2fLdvp6n8R1oV0dbfEI00qqF1GuS8";
+
+  const onToken = (token) => {
+    console.log(token);
+    alert("Payment Successful!");
+  };
+
+  return (
+    <StripeCheckout
+      label="Pay Now"
+      name="CRWN Clothing Ltd."
+      billingAddress
+      shippingAddress
+      image="https://svgshare.com/i/CUz.svg"
+      description={`Your total is $${price}`}
+      amount={priceForStripe}
+      panelLabel="Pay Now"
+      token={onToken}
+      stripeKey={publishableKey}
+    />
+  );
+};
+
+export default StripeCheckoutButton;
