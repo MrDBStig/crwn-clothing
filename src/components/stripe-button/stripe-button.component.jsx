2c3cae4fab32c8653e8d9df17fc65d3a12e15d5c
Mikita Kavaleuski <nikcrafter1999@gmail.com>
2022-02-05 11:42:51 +0300

    Implemented styled components, converted out pages and components into styled, deleted scss files

--- src/components/stripe-button/stripe-button.component.jsx
+++ src/components/stripe-button/stripe-button.component.jsx
@@ -8,7 +8,7 @@ const StripeCheckoutButton = ({ price }) => {
 
   const onToken = (token) => {
     console.log(token);
-    alert("Payment Successful!");
+    alert("Payment Succesful!");
   };
 
   return (
